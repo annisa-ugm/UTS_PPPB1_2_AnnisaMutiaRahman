ec179733a3b63a673bad220350c47dde8319413b
annisa-ugm <annisamutiarahman@mail.ugm.ac.id>
2024-10-09 11:49:52 +0700

    up

--- app/src/main/java/com/example/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan6/MainActivity.kt
@@ -13,6 +13,7 @@ import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -21,9 +22,9 @@ class MainActivity : AppCompatActivity() {
         with(binding){
 //            Get Array
             val monthList = resources.getStringArray(R.array.month)
-            val adapterSpinnerMonth = ArrayAdapter(this@MainActivity,
-                android.R.layout.simple_spinner_dropdown_item, monthList)
-            datepicker.month = adapterSpinnerMonth[]
+//            val adapterSpinnerMonth = ArrayAdapter(this@MainActivity,
+//                android.R.layout.simple_spinner_dropdown_item, monthList)
+//            datepicker.month = adapterSpinnerMonth[]
 
 //            Initiate
             var selectedTime ="${timePicker.hour}:${timePicker.minute}"
@@ -36,8 +37,10 @@ class MainActivity : AppCompatActivity() {
             val kehadiranList = listOf("tepat waktu", "terlambat", "izin")
             val adapterKehadiran = ArrayAdapter(this@MainActivity,
                 android.R.layout.simple_spinner_item, kehadiranList)
+            adapterKehadiran.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             kehadiranSpinner.adapter = adapterKehadiran
 
+            keteranganEdittext.visibility = View.GONE
 //            Selected Kehadiran
             kehadiranSpinner.onItemSelectedListener =
                 object : AdapterView.OnItemSelectedListener {
@@ -46,6 +49,10 @@ class MainActivity : AppCompatActivity() {
                         view: View?,
                         position: Int,
                         id: Long) {
+                        when (position) {
+                            0 -> keteranganEdittext.visibility = View.GONE
+                            1, 2 -> keteranganEdittext.visibility = View.VISIBLE
+                        }
 
                     }
 
@@ -55,6 +62,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
             submitButton.setOnClickListener {
+                selectedTime = "${timePicker.hour}:${timePicker.minute}"
                 Toast.makeText(this@MainActivity, "Presensi berhasil $selectedDate jam $selectedTime" , Toast.LENGTH_SHORT).show()
             }
 
